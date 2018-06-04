4ade061bc266459c74372b2856f464a5c9739cdd
Raghu Kaippully <rkaippully@gmail.com>
2018-06-04 15:48:30 +0530

    Fix ghr version

--- tools/install-ghr.sh
+++ tools/install-ghr.sh
@@ -11,7 +11,7 @@ else
         ARCH="darwin"
     fi
   echo "Installing ghr"
-  URL="https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.10.0_${ARCH}_386.zip"
+  URL="https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_${ARCH}_386.zip"
   curl -L ${URL} > ghr.zip
   mkdir -p "$HOME/bin"
   export PATH="$HOME/bin:$PATH"
